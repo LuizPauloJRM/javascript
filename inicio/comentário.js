99818ff8694df11a3979c167e6e6866d8faeab14
lpaulodesenvolver <129099980+lpaulodesenvolver@users.noreply.github.com>
2023-05-30 19:36:08 -0300

    atualização README

---
+++ inicio/comentário.js
@@ -0,0 +1,11 @@
+//console.log é usado para exibir algo na tela do computador
+console.log("passo #1");
+console.log("passo #2");//mais um exemplo de console.log!
+console.log("passo #3");
+//console.log("passo #2")
+/*
+Esse é um 
+comentario de 
+múltiplas linhas
+*/
+console.log('passo #2');
